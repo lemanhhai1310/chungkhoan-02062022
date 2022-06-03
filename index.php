638d2d9a740a9ced806ff3b218a66b2473e3dbb4
hailm <hai.lemanh.1310@gmail.com>
2022-06-03 08:01:41 +0700

    update

--- index.php
+++ index.php
@@ -1,9 +1,65 @@
 <?php $data["title"] = "Trang chủ"; ?>
 <?php $body = '' ?>
 <?php require "template-parts/layouts/header.php"; ?>
-<!--CHƯƠNG TRÌNH ƯU ĐÃI-->
-<div class="uk-section uk-background-norepeat uk-background-top-center uk-background-cover" data-src="images/capture-recovered-20220421114429.jpeg" uk-img>
+<!--SẢN PHẨM ĐẦU TƯ-->
+<div class="home__blockSanpham uk-light uk-section uk-background-norepeat uk-background-top-left uk-background-cover" data-src="images/capture-recovered-20220421114429.jpeg" uk-img>
+    <div class="uk-container">
+        <div class="home__blockSanpham__item">
+            <h3 class="uk-h3 uk-text-center uk-text-uppercase home__title">
+                SẢN PHẨM ĐẦU TƯ <br class="uk-visible@m">
+                TẠI CHỨNG KHOÁN TECHCOMBANK
+            </h3>
+        </div>
+        <div class="home__blockSanpham__item">
+            <div class="uk-grid uk-child-width-1-3@m uk-grid-match" uk-grid>
+                <div>
+                    <div class="uk-card uk-card-body uk-card-default">
 
+                    </div>
+                </div>
+                <div>
+                    <div class="uk-card uk-card-body uk-card-default">
+
+                    </div>
+                </div>
+                <div>
+                    <div class="uk-card uk-card-body uk-card-default">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--/SẢN PHẨM ĐẦU TƯ-->
+
+<!--CHƯƠNG TRÌNH ƯU ĐÃI-->
+<div class="home__blockChuongtrinh uk-light uk-section uk-background-norepeat uk-background-top-center uk-background-cover" data-src="images/capture-recovered-20220421114429.jpeg" uk-img>
+    <div class="uk-container">
+        <div class="home__blockChuongtrinh__item">
+            <h3 class="uk-h3 uk-text-center uk-text-uppercase home__title">
+                CHƯƠNG TRÌNH ƯU ĐÃI <br class="uk-visible@m">
+                KHI MỞ MỚI TÀI KHOẢN CHỨNG KHOÁN TECHCOMBANK
+            </h3>
+        </div>
+        <div class="home__blockChuongtrinh__item">
+            <div class="uk-flex-middle uk-flex-center uk-child-width-auto@m" uk-grid>
+                <div>
+                    <img style="width: 418px" src="images/big-icon12x-20211217163600.png" alt="">
+                </div>
+                <div>
+                    <ul class="uk-list home__blockChuongtrinh__list">
+                        <li>Miễn phí tới <span>200 triệu</span> phí giao dịch</li>
+                        <li>Nhận ngay <span>100 iXu</span> khi mở thành công</li>
+                        <li>Tài khoản <span>VIP</span>, số tài khoản đẹp theo sở thích</li>
+                        <li>Hỗ trợ mở tài khoản TCBS ngay tại nhà</li>
+                        <li>Đăng ký Online nhanh gọn sau 3 phút</li>
+                    </ul>
+                    <a target="_blank" href="https://tcinvest.tcbs.com.vn/guest/login?openAccount=true&IWPCode=105C988916" class="home__blockChuongtrinh__btn uk-button uk-button-default">ĐĂNG KÝ MỞ THEO CHƯƠNG TRÌNH</a>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 <!--/CHƯƠNG TRÌNH ƯU ĐÃI-->
 <?php require "template-parts/layouts/footer.php"; ?>
