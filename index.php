201db6a2d5a6e4584f06f13ac745cc669fa8d384
hailm <hai.lemanh.1310@gmail.com>
2022-06-03 07:14:19 +0700

    update

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php $data["title"] = "Trang chủ"; ?>
 <?php $body = '' ?>
 <?php require "template-parts/layouts/header.php"; ?>
+<!--CHƯƠNG TRÌNH ƯU ĐÃI-->
+<div class="uk-section uk-background-norepeat uk-background-top-center uk-background-cover" data-src="images/capture-recovered-20220421114429.jpeg" uk-img>
 
+</div>
+<!--/CHƯƠNG TRÌNH ƯU ĐÃI-->
 <?php require "template-parts/layouts/footer.php"; ?>
--- template-parts/layouts/footer.php
+++ template-parts/layouts/footer.php
@@ -1,3 +1,35 @@
+<!--footer-->
+<footer class="uk-section-small footer uk-light">
+    <div class="uk-container">
+        <div class="uk-grid-small uk-flex-between" uk-grid>
+            <div class="uk-width-2-5@m">
+                <div class="footer__item">
+                    <h4 class="uk-h4 footer__title">CÔNG TY CỔ PHẦN CHỨNG KHOÁN KỸ THƯƠNG</h4>
+                </div>
+                <div class="footer__item">
+                    <ul class="uk-list footer__list">
+                        <li>Địa chỉ: Tầng 10 và Tầng 12, Tòa nhà Techcombank, 191 Bà Triệu, P. Lê Đại Hành, Q. Hai Bà Trưng Hà Nội</li>
+                        <li>Hotline: 1800 588 826</li>
+                        <li>Website mở tài khoản ưu đãi của iwp TCBS</li>
+                        <li>Email: cskh@tcbs.com.vn</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="uk-width-auto@m">
+                <div class="footer__item">
+                    <h4 class="uk-h4 footer__title1">Kết nối với chúng tôi</h4>
+                </div>
+                <div class="footer__item">
+                    <ul class="uk-grid uk-grid-10 uk-flex-right@m">
+                        <li><a href="https://www.facebook.com/techcomsecurities/" target="_blank" class="footer__socialIcon footer__socialIcon--fb"></a></li>
+                        <li><a href="https://www.youtube.com/channel/UCkCFfo54a9WQNJQiKV18z2A" target="_blank" class="footer__socialIcon footer__socialIcon--youtube"></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!--/footer-->
 </div>
 <!--/app-->
 </body>
